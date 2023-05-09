dbbad110e0ee0d801e4378e0d327729197ad5dbd
MohamedFathi96 <mohamedfathi4345@gmail.com>
2023-05-09 20:07:38 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -10,16 +10,56 @@ const closeMenu = document.querySelector(".menu-close");
 const headerModal = document.getElementById("header-modal");
 const headerButton = document.querySelector(".menu");
 const nationalityDiv = document.querySelector(".nationality-div");
+const destinationList = document.getElementById("destination-list");
+const destinationInput = document.getElementById("dist");
+
+document.addEventListener("click", (e) => {
+  if (e.target.matches("#dist") || e.target.matches("#nationality")) return;
+  nationalityList.classList.add("hidden");
+  destinationList.classList.add("hidden");
+});
 
 nationalityInput.addEventListener("focus", () => {
+  destinationList.classList.add("hidden");
   nationalityList.classList.remove("hidden");
 });
-nationalityInput.addEventListener("focusout", () => {
+destinationInput.addEventListener("focus", () => {
   nationalityList.classList.add("hidden");
+  destinationList.classList.remove("hidden");
 });
 
-nationalityList.addEventListener("click", () => {
-  console.log("test");
+destinationInput.addEventListener("input", (elem) => {
+  emptyDestinatinList();
+  updateDestinationDebounce(elem.target.value);
+});
+
+nationalityInput.addEventListener("input", (elem) => {
+  emptyNationalityList();
+  updateNationalityDebounce(elem.target.value);
+});
+
+function debounce(cb, delay = 300) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
+
+const updateDestinationDebounce = debounce(async (searchText) => {
+  const res = await fetch(
+    `https://restcountries.com/v3.1/name/${searchText}?fields=name,flags`
+  );
+  if (res.ok) {
+    const data = await res.json();
+    data.forEach((suggestionResponse) => {
+      createNewDestinationItemLIst(suggestionResponse);
+    });
+  } else {
+    emptyNationalityList();
+  }
 });
 
 const updateNationalityDebounce = debounce(async (searchText) => {
@@ -29,40 +69,54 @@ const updateNationalityDebounce = debounce(async (searchText) => {
   if (res.ok) {
     const data = await res.json();
     data.forEach((suggestionResponse) => {
-      createNewitemList(suggestionResponse);
+      createNewNationalityitemList(suggestionResponse);
     });
   } else {
-    while (nationalityList.hasChildNodes()) {
-      nationalityList.firstChild.remove();
-    }
+    emptyNationalityList();
   }
 });
 
-function createNewitemList(suggestionResponse) {
+function createNewDestinationItemLIst(suggestionResponse) {
+  const newListItem = document.createElement("li");
+  newListItem.append(document.createTextNode(suggestionResponse.name.common));
+  destinationList.append(newListItem);
+  newListItem.addEventListener("click", userClickedDestination);
+  console.log(newListItem);
+}
+
+function userClickedDestination(e) {
+  console.log(e.target);
+}
+
+function createNewNationalityitemList(suggestionResponse) {
   const newListItem = document.createElement("li");
   const flagImg = document.createElement("img");
   flagImg.src = suggestionResponse.flags.png;
   newListItem.append(flagImg);
   newListItem.append(document.createTextNode(suggestionResponse.name.common));
   nationalityList.append(newListItem);
+  newListItem.addEventListener("click", userClickedNationality);
 }
 
-function userClickedNationality() {
-  console.log("test");
+function userClickedNationality(e) {
+  let oldImage = document.querySelector(".flag");
+  if (oldImage) oldImage.remove();
+  const imgFlag = e.target.childNodes[0];
+  imgFlag.classList.add("flag");
+  nationalityInput.value = e.target.childNodes[1].nodeValue;
+  nationalityDiv.prepend(imgFlag);
+  emptyNationalityList();
 }
 
-nationalityInput.addEventListener("input", (elem) => {
-  updateNationalityDebounce(elem.target.value);
-});
-
-function debounce(cb, delay = 500) {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      cb(...args);
-    }, delay);
-  };
+function emptyNationalityList() {
+  while (nationalityList.hasChildNodes()) {
+    nationalityList.firstChild.remove();
+  }
+}
+function emptyDestinatinList() {
+  while (destinationList.hasChildNodes()) {
+    destinationList.firstChild.remove();
+  }
 }
 
 function updateNights() {
